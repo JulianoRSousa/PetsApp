381daf88b3732694de45af12a352de39932a9359
Juliano Sousa <julianorodosusa@gmail.com>
2021-05-16 07:03:09 -0400

    TabBar Screens created

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import HomeScreen from '../pages/HomeScreen';
 import Profile from '../pages/Profile';
+import Adoption from '../pages/Adoption';
+import Friends from '../pages/Friends';
 
 
 const AppTab = createBottomTabNavigator();
@@ -9,7 +11,9 @@ const AppTab = createBottomTabNavigator();
 const AppRoutes: React.FC = () => (
   <AppTab.Navigator>
     <AppTab.Screen name="HomeScreen" component={HomeScreen} options={{ tabBarBadge: 'oloko', tabBarLabel: 'Inicio', }} />
-    <AppTab.Screen name="Amigos" component={Profile} options={{ title: 'Amigos' }} />
+    <AppTab.Screen name="Friends" component={Friends} options={{ title: 'Amigos' }} />
+    <AppTab.Screen name="Adoption" component={Adoption} options={{ title: 'Adoção' }} />
+    <AppTab.Screen name="Profile" component={Profile} options={{ title: 'Meu Perfil' }} />
   </AppTab.Navigator>
 );
 
