b124c7ed5d368603a9aba40e4924b8ee565b6234
julianorodsousa <julianorodsousa@gmail.com>
2021-06-01 15:05:27 -0400

    Added Icons

--- src/pages/HomeScreen/index.tsx
+++ src/pages/HomeScreen/index.tsx
@@ -1,7 +1,8 @@
 import React from "react";
-import { ScrollView, StyleSheet} from "react-native";
+import { ScrollView, StyleSheet } from "react-native";
 import HeaderInfo from '../../components/HeaderInfo';
 import Lottie from 'lottie-react-native';
+import IconMaterial from 'react-native-vector-icons/MaterialCommunityIcons';
 
 const styles = StyleSheet.create({
   container: {
@@ -13,45 +14,42 @@ const HomeScreen: React.FC = () => {
 
   return (
     <ScrollView style={styles.container}>
-      <HeaderInfo/>
+      <HeaderInfo />
       <Lottie
-      source={require('../../assets/animations/dog-caramelo.json')}
-      autoPlay
-      loop
-      speed={1.2}
-      style={{height:200, width:200}}
+        source={require('../../assets/animations/dog-caramelo.json')}
+        autoPlay
+        loop
+        speed={1.2}
+        style={{ height: 200, width: 200 }}
       />
       <Lottie
-      source={require('../../assets/animations/dog-caramelo.json')}
-      autoPlay
-      loop
-      speed={1.2}
-      style={{height:200, width:200}}
+        source={require('../../assets/animations/dog-caramelo.json')}
+        autoPlay
+        loop
+        speed={1.2}
+        style={{ height: 200, width: 200 }}
       />
       <Lottie
-      source={require('../../assets/animations/dog-caramelo.json')}
-      autoPlay
-      loop
-      speed={1.2}
-      style={{height:200, width:200}}
+        source={require('../../assets/animations/dog-caramelo.json')}
+        autoPlay
+        loop
+        speed={1.2}
+        style={{ height: 200, width: 200 }}
       />
       <Lottie
-      source={require('../../assets/animations/dog-caramelo.json')}
-      autoPlay
-      loop
-      speed={1.2}
-      style={{height:200, width:200}}
+        source={require('../../assets/animations/dog-caramelo.json')}
+        autoPlay
+        loop
+        speed={1.2}
+        style={{ height: 200, width: 200 }}
       />
-<<<<<<< HEAD
-=======
       <Lottie
-      source={require('../../assets/animations/dog-caramelo.json')}
-      autoPlay
-      loop
-      speed={1.2}
-      style={{height:200, width:200}}
+        source={require('../../assets/animations/dog-caramelo.json')}
+        autoPlay
+        loop
+        speed={1.2}
+        style={{ height: 200, width: 200 }}
       />
->>>>>>> 7cee80ff743b230215ee1130819c8074a120c7ab
     </ScrollView>
   );
 };
