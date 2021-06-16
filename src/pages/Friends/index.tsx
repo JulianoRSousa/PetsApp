719de2fe8ce45c7c1b556dfdf1c5c336793d49a4
julianorodsousa <julianorodsousa@gmail.com>
2021-06-16 18:08:07 -0400

    search Style changed  (2)

--- src/pages/Friends/index.tsx
+++ src/pages/Friends/index.tsx
@@ -9,11 +9,11 @@ const Friends: React.FC = () => {
   const navigation = useNavigation();
 
   const [tags, setTags] = useState('');
-  const [resultSearch, setResultSearch] = useState<any>(null);
+  const [resultSearch, setResultSearch] = useState<any>([]);
   const [searched, setSearched] = useState(false)
 
-  async function Search() {
-    !searched ? setSearched(true) : console.log('Go == false')
+  async function Search(tags) {
+    !searched ? setSearched(true) : (tags.length != 0 ? console.log('not go') : setSearched(false))
     await api.get('/searchFriends', {
       headers: {
         tags: tags,
@@ -29,44 +29,61 @@ const Friends: React.FC = () => {
     })
   }
 
-  function resultView(){
-    if(resultSearch.length){
-      return(
-        <FlatList
-          data={resultSearch}
-          keyExtractor={item => item._id}
-          renderItem={({ item }) => (
-            <TouchableOpacity onPress={() => OpenUserProfile(item._id)} style={{ alignItems: 'center', flexDirection: 'row', width: "100%", backgroundColor: '#333d', borderTopWidth: 1, borderColor: '#fff' }}>
-              <Image
-                source={{ uri: item.picture_url }}
-                style={{ resizeMode: 'contain', height: 50, width: 50, borderRadius: 50, margin: 8 }}
-              />
-              <View>
-                <Text
-                  style={{
-                    color: 'white',
-                    fontSize: 15,
-                    marginHorizontal: 10,
-                  }}>{' ' + String(item.firstName) + ' ' + item.lastName}
-                </Text>
-                <Text
-                  style={{
-                    color: 'white',
-                    fontSize: 12,
-                    marginHorizontal: 10,
-                    marginVertical: 5,
-                  }}>
-                  {' @' + item.username}
-                </Text>
-              </View>
-            </TouchableOpacity>
-          )}
-        />
+  function resultView() {
+    if (resultSearch.length) {
+      return (
+        <View>
+          <FlatList
+            data={resultSearch}
+            ListHeaderComponent={<View style={{
+              height: 20, borderTopStartRadius: 50, borderTopEndRadius: 50,
+              backgroundColor: '#333d'
+            }}></View>}
+            ListFooterComponent={<View style={{
+              height: 20, borderBottomStartRadius: 50, borderBottomEndRadius: 50,
+              backgroundColor: '#333d'
+            }}></View>}
+            keyExtractor={item => item._id}
+            renderItem={({ item }) => (
+              <TouchableOpacity onPress={() => OpenUserProfile(item._id)} style={{
+                alignItems: 'center',
+                flexDirection: 'row', width: "100%",
+                backgroundColor: '#333d',
+                borderBottomWidth: 1,
+                borderTopWidth: 1,
+                borderColor: '#3338'
+              }}>
+                <Image
+                  source={{ uri: item.picture_url }}
+                  style={{ resizeMode: 'contain', height: 50, width: 50, borderRadius: 50, margin: 8 }}
+                />
+                <View>
+                  <Text
+                    style={{
+                      color: 'white',
+                      fontSize: 15,
+                      marginHorizontal: 10,
+                    }}>{' ' + String(item.firstName) + ' ' + item.lastName}
+                  </Text>
+                  <Text
+                    style={{
+                      color: 'white',
+                      fontSize: 12,
+                      marginHorizontal: 10,
+                      marginVertical: 5,
+                    }}>
+                    {' @' + item.username}
+                  </Text>
+                </View>
+              </TouchableOpacity>
+            )}
+          />
+        </View>
       )
     } else {
-      return(
+      return (
         <View style={{ flex: 1, width: '100%', alignSelf: 'center', borderRadius: 50, justifyContent: 'flex-start' }}>
-        { searched ? <Text>Nenhum Resultado encontrado</Text> : <View/>}
+          {searched ? <Text style={{marginVertical: 50 ,alignSelf:'center', fontSize: 14}}>Nenhum Resultado encontrado</Text> : <View />}
         </View>
       )
     }
@@ -75,13 +92,19 @@ const Friends: React.FC = () => {
 
   return (
     <View style={{ backgroundColor: '#ff8637', flex: 1 }}>
-      <View style={{ alignSelf: 'center', borderTopEndRadius: 20, borderTopStartRadius: 20, height: '10%', width: '90%', flexDirection: 'row', backgroundColor: '#fff' }}>
-        <TextInput style={{ paddingHorizontal: 25, width: '82%', }}
+      <View style={{ alignSelf: 'center', borderRadius: 50, height: '10%', width: '90%', flexDirection: 'row', backgroundColor: '#fff' }}>
+        <TextInput style={{ paddingHorizontal: 25, width: '84%', }}
           value={tags}
-          onChangeText={text => { setTags(text), Search()}}
+          onChangeText={text => { setTags(text), Search(text) }}
           placeholder={'Buscar Amigos'}
-          onSubmitEditing={() => Search()}></TextInput>
-        <TouchableOpacity style={{ height: 50, width: 50, alignSelf: 'center', borderRadius: 50, alignItems: 'center', justifyContent: 'center', backgroundColor: '#35a' }} onPress={() => Search()}>
+          onSubmitEditing={() => Search(tags)}></TextInput>
+        <TouchableOpacity style={{ height: 50, 
+          width: 50, 
+          alignSelf: 'center', 
+          borderRadius: 50, 
+          alignItems: 'center', 
+          justifyContent: 'center', 
+          backgroundColor: '#35a' }} onPress={() => Search(tags)}>
           <Text style={{ borderRadius: 50, color: '#fff' }}>Go</Text>
         </TouchableOpacity>
       </View>
